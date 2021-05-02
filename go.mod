1e8930c2743dab7c4cc0abc709df75896e40024b
Dave Williams <dave@dave.io>
2021-05-02 19:54:41 +0100

    it's alive

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-resty/resty/v2 v2.6.0
 	github.com/prometheus/client_golang v1.10.0
 	github.com/sirupsen/logrus v1.8.1
+	mvdan.cc/gofumpt v0.1.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"errors"
 	"flag"
-	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/go-resty/resty/v2"
@@ -13,47 +14,131 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var (
-	refreshSecs       = 60
-	aaControlUsername = os.Getenv("AAISP_CONTROL_USERNAME")
-	aaControlPassword = os.Getenv("AAISP_CONTROL_PASSWORD")
-	httpClient        = resty.New()
-	gaugeLabels       = []string{
-		"LineID",
-		"Login",
-		"Postcode",
-	}
-	upstreamGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "upstream_sync_rate",
-		Help: "Raw upstream sync rate (bits/sec)",
-	}, gaugeLabels)
-	downstreamGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "downstream_sync_rate",
-		Help: "Raw downstream sync rate (bits/sec)",
-	}, gaugeLabels)
-	adjustedDownstreamGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "adjusted_downstream_rate",
-		Help: "Adjusted downstream rate after optional rate limiting (bits/sec)",
-	}, gaugeLabels)
-	monthlyQuotaGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "monthly_allowance",
-		Help: "Monthly quota (bytes)",
-	}, gaugeLabels)
-	quotaRemainingGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "monthly_allowance_remaining",
-		Help: "Quota remaining, may exceed monthly_allowance due to rollover of unused quota (bytes)",
-	}, gaugeLabels)
-	gauges = []prometheus.GaugeVec{
-		*upstreamGauge, *downstreamGauge, *adjustedDownstreamGauge, *monthlyQuotaGauge, *quotaRemainingGauge,
-	}
-	registry = prometheus.NewRegistry()
-)
-
 type AaResponse struct {
-	Info []map[string]string `json:"info"`
+	Info []AaLineData `json:"info"`
 }
 
-func GetUpdatedValues() {
+type AaLineData struct {
+	LineID         string `json:"id"`
+	QuotaMonthly   string `json:"quota_monthly"`
+	QuotaRemaining string `json:"quota_remaining"`
+	RxRate         string `json:"rx_rate"`
+	TxRate         string `json:"tx_rate"`
+	TxRateAdjusted string `json:"tx_rate_adjusted"`
+}
+
+type AaGauges struct {
+	QuotaMonthly   prometheus.GaugeVec
+	QuotaRemaining prometheus.GaugeVec
+	RxRate         prometheus.GaugeVec
+	TxRate         prometheus.GaugeVec
+	TxRateAdjusted prometheus.GaugeVec
+}
+
+func main() {
+	flag.Parse()
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors: false, DisableColors: false, ForceQuote: false, DisableQuote: false, EnvironmentOverrideColors: false,
+		DisableTimestamp: false, FullTimestamp: false, TimestampFormat: "", DisableSorting: false, SortingFunc: nil,
+		DisableLevelTruncation: false, PadLevelText: false, QuoteEmptyFields: false, FieldMap: nil, CallerPrettyfier: nil,
+	})
+
+	var (
+		refreshSecs   = 60
+		gaugeLabels   = []string{"LineID"}
+		upstreamGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "", Subsystem: "", ConstLabels: nil,
+			Name: "upstream_sync_rate",
+			Help: "Raw upstream sync rate (bits/sec)",
+		}, gaugeLabels)
+		downstreamGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "", Subsystem: "", ConstLabels: nil,
+			Name: "downstream_sync_rate",
+			Help: "Raw downstream sync rate (bits/sec)",
+		}, gaugeLabels)
+		adjustedDownstreamGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "", Subsystem: "", ConstLabels: nil,
+			Name: "downstream_rate_adjusted",
+			Help: "Adjusted downstream rate after optional rate limiting (bits/sec)",
+		}, gaugeLabels)
+		monthlyQuotaGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "", Subsystem: "", ConstLabels: nil,
+			Name: "monthly_allowance",
+			Help: "Monthly quota (bytes)",
+		}, gaugeLabels)
+		quotaRemainingGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "", Subsystem: "", ConstLabels: nil,
+			Name: "monthly_allowance_remaining",
+			Help: "Quota remaining, may exceed monthly_allowance due to rollover of unused quota (bytes)",
+		}, gaugeLabels)
+		gauges = AaGauges{
+			QuotaMonthly:   *monthlyQuotaGauge,
+			QuotaRemaining: *quotaRemainingGauge,
+			RxRate:         *upstreamGauge,
+			TxRate:         *downstreamGauge,
+			TxRateAdjusted: *adjustedDownstreamGauge,
+		}
+		registry = prometheus.NewRegistry()
+	)
+
+	// Register the gauges with Prometheus's default registry.
+	for _, gauge := range []prometheus.GaugeVec{
+		gauges.QuotaMonthly,
+		gauges.QuotaRemaining,
+		gauges.RxRate,
+		gauges.TxRate,
+		gauges.TxRateAdjusted,
+	} {
+		registry.MustRegister(gauge)
+	}
+	// Add Go module build info.
+	registry.MustRegister(prometheus.NewBuildInfoCollector())
+
+	// Update CHAOS data every 60 seconds.
+	go ScheduleUpdates(gauges, refreshSecs)
+
+	// Expose the registered metrics via HTTP.
+	http.Handle("/metrics", promhttp.HandlerFor(
+		registry,
+		promhttp.HandlerOpts{
+			ErrorLog: nil, ErrorHandling: 0, Registry: nil, DisableCompression: false, MaxRequestsInFlight: 0, Timeout: 0,
+			EnableOpenMetrics: true,
+		},
+	))
+	logrus.Fatal(http.ListenAndServe(":2112", nil))
+}
+
+func ScheduleUpdates(gauges AaGauges, refreshSecs int) {
+	for {
+		err, vals := GetUpdatedValues()
+		if err != nil {
+			logrus.Error("Scheduled update failed")
+		} else {
+			if len(vals.Info) < 1 {
+				logrus.Error("No data returned from CHAOS API")
+			} else {
+				for _, lineVal := range vals.Info {
+					UpdateGauge(lineVal.QuotaMonthly, lineVal.LineID, &gauges.QuotaMonthly)
+					UpdateGauge(lineVal.QuotaRemaining, lineVal.LineID, &gauges.QuotaRemaining)
+					UpdateGauge(lineVal.RxRate, lineVal.LineID, &gauges.RxRate)
+					UpdateGauge(lineVal.TxRate, lineVal.LineID, &gauges.TxRate)
+					UpdateGauge(lineVal.TxRateAdjusted, lineVal.LineID, &gauges.TxRateAdjusted)
+				}
+			}
+		}
+
+		time.Sleep(
+			time.Duration(refreshSecs) * time.Second)
+	}
+}
+
+func GetUpdatedValues() (error, AaResponse) {
+	var (
+		aaControlUsername = os.Getenv("AAISP_CONTROL_USERNAME")
+		aaControlPassword = os.Getenv("AAISP_CONTROL_PASSWORD")
+		httpClient        = resty.New()
+	)
+
 	resp, err := httpClient.
 		R().
 		SetHeader("Content-Type", "application/json").
@@ -62,56 +147,27 @@ func GetUpdatedValues() {
 			"control_login":    aaControlUsername,
 			"control_password": aaControlPassword,
 		}).
-		SetResult(AaResponse{}).
+		SetResult(AaResponse{Info: nil}).
 		Post("https://chaos2.aa.net.uk/broadband/info/json")
 
 	if err != nil {
 		if resp != nil {
-			logrus.Error(resp.String())
+			logrus.Error(err.Error())
+			return errors.New(err.Error()), AaResponse{}
 		} else {
-			logrus.Error("Unknown failure fetching updated data")
+			logrus.Error("Unknown failure fetching update")
+			return errors.New(err.Error()), AaResponse{}
 		}
 	} else {
-		data := resp.Result()
-		logrus.WithField("data", data).Info("Update successful")
+		return nil, *resp.Result().(*AaResponse)
 	}
 }
 
-func main() {
-	flag.Parse()
-	logrus.SetFormatter(&logrus.TextFormatter{})
-	// Register the summary and the histogram with Prometheus's default registry.
-	for _, gauge := range gauges {
-		registry.MustRegister(gauge)
+func UpdateGauge(valStr string, lineID string, gauge *prometheus.GaugeVec) {
+	valFloat, err := strconv.ParseFloat(valStr, 64)
+	if err == nil {
+		gauge.With(prometheus.Labels{
+			"LineID": lineID,
+		}).Set(valFloat)
 	}
-	// Add Go module build info.
-	registry.MustRegister(prometheus.NewBuildInfoCollector())
-
-	// Update CHAOS data every 60 seconds.
-	go func() {
-		for {
-			GetUpdatedValues()
-			// generate fake data
-			for _, gauge := range gauges {
-				gauge.With(prometheus.Labels{
-					"LineID":   "1234",
-					"Login":    "dw121@a.255",
-					"Postcode": "N7 7EL",
-				}).Set(rand.NormFloat64())
-			}
-
-			time.Sleep(
-				time.Duration(refreshSecs) * time.Second)
-		}
-	}()
-
-	// Expose the registered metrics via HTTP.
-	http.Handle("/metrics", promhttp.HandlerFor(
-		registry,
-		promhttp.HandlerOpts{
-			// Opt into OpenMetrics to support exemplars.
-			EnableOpenMetrics: true,
-		},
-	))
-	logrus.Fatal(http.ListenAndServe(":2112", nil))
 }
